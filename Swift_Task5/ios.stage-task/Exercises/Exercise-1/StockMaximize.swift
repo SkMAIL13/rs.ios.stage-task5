e5e63425364034eede9dd53b88449d76bcf97ef9
Kamran <iskama98@mail.ru>
2021-06-13 19:14:56 +0300

    feat: done exercise-1

--- Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
@@ -3,6 +3,33 @@ import Foundation
 class StockMaximize {
 
     func countProfit(prices: [Int]) -> Int {
-        return -1
+        if prices.count == 0 || prices.count == 1 { return 0 }
+        var buy = [0, 0]
+        var benefit = 0
+        for i in 0..<prices.count {
+            for j in i+1..<prices.count {
+                let maxPrice = prices[j..<prices.count].max() ?? 0
+                let maxIndex = prices.firstIndex(of: maxPrice) ?? 0
+                if prices[i] >= maxPrice {
+                    break
+                }
+                // Buy
+                if (prices[i] > prices[j] && prices[i] < maxPrice && i < maxIndex) ||
+                    prices[i] < prices[j] && prices[j] < maxPrice && i < maxIndex {
+                    buy[0] += 1
+                    buy[1] += prices[i]
+                }
+                // Buy and consider
+                else if prices[i] < prices[j] {
+                    benefit += prices[j] - prices[i]
+                    break
+                }
+                // Sell all
+                else if prices[i] == maxPrice {
+                    benefit += (buy[0] * prices[j]) - (buy[0] * buy[1])
+                }
+            }
+        }
+        return benefit
     }
 }
